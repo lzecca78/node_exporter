da02f7a25ab52d4ab88708ed1c239ad486ae67a7
Luca Zecca <l.zecca78@gmail.com>
2019-11-20 15:34:53 +0100

    [WIP] adding machineid collector

---
+++ collector/machine_id.go
@@ -0,0 +1,55 @@
+// Copyright 2019 The Prometheus Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build darwin freebsd openbsd linux
+// +build !nomachineid
+
+package collector
+
+import (
+	"github.com/denisbrodbeck/machineid"
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+var machineIDDesc = prometheus.NewDesc(
+	prometheus.BuildFQName(namespace, "machineid", "info"),
+	"unique machine exadecimal 32-character id",
+	[]string{
+		"machine_id",
+	},
+	nil,
+)
+
+type machineIDCollector struct{}
+
+func init() {
+	registerCollector("machineid", defaultEnabled, newMachineIDCollector)
+}
+
+// NewMachineIDCollector returns new machineIDCollector.
+func newMachineIDCollector() (Collector, error) {
+	return &machineIDCollector{}, nil
+}
+
+func (c *machineIDCollector) Update(ch chan<- prometheus.Metric) error {
+	machineID, err := machineid.ID()
+	if err != nil {
+		return err
+	}
+
+	ch <- prometheus.MustNewConstMetric(machineIDDesc, prometheus.GaugeValue, 1,
+		machineID,
+	)
+
+	return nil
+}
